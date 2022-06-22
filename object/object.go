c2a232b5740e959fdc4baf15ec75e1abca1352ab
Daniel Ratmiroff <danielratmiroff@gmail.com>
2022-06-22 19:15:24 +0200

    environment values added

--- object/object.go
+++ object/object.go
@@ -3,7 +3,10 @@ package object
 // Object system
 
 import (
+	"bytes"
 	"fmt"
+	"ratmy/ast"
+	"strings"
 )
 
 type ObjectType string
@@ -14,14 +17,22 @@ type Object interface {
 }
 
 const (
+	FUNCTION_OBJ     = "FUNCTION"
 	INTEGER_OBJ      = "INTEGER"
 	BOOLEAN_OBJ      = "BOOLEAN"
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
+	ERROR_OBJ        = "ERROR"
 )
 
 type Integer struct {
 	Value int64
+}
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
 }
 
 type Boolean struct {
@@ -30,9 +41,30 @@ type Boolean struct {
 
 type Null struct {
 }
+type Environment struct {
+	store map[string]Object
+}
 
 type ReturnValue struct {
 	Value Object
+}
+
+type Error struct {
+	Message string
+}
+
+func NewEnvironment() *Environment {
+	s := make(map[string]Object)
+	return &Environment{store: s}
+}
+
+func (e *Environment) Get(name string) (Object, bool) {
+	obj, ok := e.store[name]
+	return obj, ok
+}
+func (e *Environment) Set(name string, val Object) Object {
+	e.store[name] = val
+	return val
 }
 
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
@@ -44,5 +76,27 @@ func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
 func (r *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
 func (r *ReturnValue) Inspect() string  { return r.Value.Inspect() }
 
+func (e *Error) Type() ObjectType { return "ERROR_OBJ" }
+func (e *Error) Inspect() string  { return "ERROR:" + e.Message }
+
 func (n *Null) Type() ObjectType { return NULL_OBJ }
 func (n *Null) Inspect() string  { return "null" }
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("{")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
--- repl/repl.go
+++ repl/repl.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"ratmy/evaluator"
 	"ratmy/lexer"
+	"ratmy/object"
 	"ratmy/parser"
 )
 
@@ -15,6 +16,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Print(PROMPT)
@@ -34,7 +36,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")

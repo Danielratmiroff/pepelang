7a7d00fc7e36ad8480f1e057adf4541bd223a75b
Daniel Ratmiroff <danielratmiroff@gmail.com>
2022-06-25 14:02:55 +0200

    hashs added

--- object/object.go
+++ object/object.go
@@ -5,6 +5,7 @@ package object
 import (
 	"bytes"
 	"fmt"
+	"hash/fnv"
 	"ratmy/ast"
 	"strings"
 )
@@ -26,7 +27,26 @@ const (
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
 	ARRAY_OBJ        = "ARRAY"
+	HASH_OBJ         = "HASH"
 )
+
+type Hashable interface {
+	HashKey() HashKey
+}
+
+type HashPair struct {
+	Key   Object
+	Value Object
+}
+
+type Hash struct {
+	Pairs map[HashKey]HashPair
+}
+
+type HashKey struct {
+	Type  ObjectType
+	Value uint64
+}
 
 type Integer struct {
 	Value int64
@@ -64,6 +84,44 @@ type ReturnValue struct {
 
 type Error struct {
 	Message string
+}
+
+func (b *Boolean) HashKey() HashKey {
+	var value uint64
+
+	if b.Value {
+		value = 1
+	} else {
+		value = 0
+	}
+	return HashKey{Type: b.Type(), Value: value}
+}
+
+func (i *Integer) HashKey() HashKey {
+	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
+
+func (s *String) HashKey() HashKey {
+	h := fnv.New64a()
+	h.Write([]byte(s.Value))
+	return HashKey{Type: s.Type(), Value: h.Sum64()}
+}
+
+func (h *Hash) Type() ObjectType { return STRING_OBJ }
+func (h *Hash) Inspect() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for _, pair := range h.Pairs {
+		pairs = append(pairs, fmt.Sprintf("%s: %s",
+			pair.Key.Inspect(), pair.Value.Inspect()))
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
+
+	return out.String()
 }
 
 func (s *String) Type() ObjectType { return STRING_OBJ }

26e6b0d3628194eb21b89cbc3dd738d0017a4085
Daniel Ratmiroff <danielratmiroff@gmail.com>
2022-07-13 18:19:15 +0200

    Progress

--- repl/repl.go
+++ repl/repl.go
@@ -19,52 +19,40 @@ const PROMPT = ">> "
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 	env := object.NewEnvironment()
-	// TODO: refactor this to make it prettier
+
 	if len(os.Args) == 2 {
 		args := os.Args[1]
 		line := fmt.Sprintf("pp %v", args)
 		input := ReadUserInput(line)
-		l := lexer.New(input)
-		p := parser.New(l)
-		program := p.ParseProgram()
-
-		if len(p.Errors()) != 0 {
-			printParseErrors(out, p.Errors())
-		}
-
-		evaluated := evaluator.Eval(program, env)
-		if evaluated != nil {
-			io.WriteString(out, evaluated.Inspect())
-			io.WriteString(out, "\n")
-		}
+		startProgram(input, out, env)
 	}
 
 	for {
-		var line string
 		fmt.Print(PROMPT)
-
 		scanned := scanner.Scan()
 		if !scanned {
 			return
 		}
 
-		line = scanner.Text()
-
+		line := scanner.Text()
 		input := ReadUserInput(line)
-		l := lexer.New(input)
-		p := parser.New(l)
-		program := p.ParseProgram()
+		startProgram(input, out, env)
+	}
+}
 
-		if len(p.Errors()) != 0 {
-			printParseErrors(out, p.Errors())
-			continue
-		}
+func startProgram(input string, out io.Writer, env *object.Environment) {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
 
-		evaluated := evaluator.Eval(program, env)
-		if evaluated != nil {
-			io.WriteString(out, evaluated.Inspect())
-			io.WriteString(out, "\n")
-		}
+	if len(p.Errors()) != 0 {
+		printParseErrors(out, p.Errors())
+	}
+
+	evaluated := evaluator.Eval(program, env)
+	if evaluated != nil {
+		io.WriteString(out, evaluated.Inspect())
+		io.WriteString(out, "\n")
 	}
 }
 
